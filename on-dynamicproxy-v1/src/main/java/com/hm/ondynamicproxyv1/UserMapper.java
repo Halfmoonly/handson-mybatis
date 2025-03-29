22c9ecafdc311cb69ea0c34094561d7334c37b51
Halfmoonly <146072976+Halfmoonly@users.noreply.github.com>
2025-03-29 23:45:19 +0800

    更新 UserMapper.java

--- on-dynamicproxy-v1/src/main/java/com/hm/ondynamicproxyv1/UserMapper.java
+++ on-dynamicproxy-v1/src/main/java/com/hm/ondynamicproxyv1/UserMapper.java
@@ -6,7 +6,8 @@ import com.hm.ondynamicproxyv1.User;
  * @author liuyanoutsee@outlook.com
  **/
 public interface UserMapper {
-    
+
+    //程序编译后参数id变成了形如arg1 arg2的形式，因此我们需要而外定义@Param注解来标记参数名
     User selectById(@Param("id") int id);
     
     User selectByName(@Param("name") String name);
